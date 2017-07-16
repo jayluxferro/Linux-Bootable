8c6e645e017b60d18317dfd6a60f4ebef5813e38
Jay Lux Ferro <jayluxferro@sperixlabs.org>
2017-07-16 06:58:40 -0400

    Fixed bug

--- build-Rufus-Rufus-Release/moc_maindialog.cpp
+++ build-Rufus-Rufus-Release/moc_maindialog.cpp
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_MainDialog_t {
-    QByteArrayData data[15];
-    char stringdata0[222];
+    QByteArrayData data[14];
+    char stringdata0[199];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -42,8 +42,7 @@ QT_MOC_LITERAL(9, 131, 17), // "updateProgressBar"
 QT_MOC_LITERAL(10, 149, 9), // "increment"
 QT_MOC_LITERAL(11, 159, 18), // "showSuccessMessage"
 QT_MOC_LITERAL(12, 178, 3), // "msg"
-QT_MOC_LITERAL(13, 182, 16), // "showErrorMessage"
-QT_MOC_LITERAL(14, 199, 22) // "on_writeButton_clicked"
+QT_MOC_LITERAL(13, 182, 16) // "showErrorMessage"
 
     },
     "MainDialog\0openImageFile\0\0"
@@ -51,8 +50,7 @@ QT_MOC_LITERAL(14, 199, 22) // "on_writeButton_clicked"
     "clearDevice\0showWritingProgress\0"
     "maxValue\0hideWritingProgress\0"
     "updateProgressBar\0increment\0"
-    "showSuccessMessage\0msg\0showErrorMessage\0"
-    "on_writeButton_clicked"
+    "showSuccessMessage\0msg\0showErrorMessage"
 };
 #undef QT_MOC_LITERAL
 
@@ -62,7 +60,7 @@ static const uint qt_meta_data_MainDialog[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-      10,   14, // methods
+       9,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -70,16 +68,15 @@ static const uint qt_meta_data_MainDialog[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   64,    2, 0x0a /* Public */,
-       3,    0,   65,    2, 0x0a /* Public */,
-       4,    0,   66,    2, 0x0a /* Public */,
-       5,    0,   67,    2, 0x0a /* Public */,
-       6,    1,   68,    2, 0x0a /* Public */,
-       8,    0,   71,    2, 0x0a /* Public */,
-       9,    1,   72,    2, 0x0a /* Public */,
-      11,    1,   75,    2, 0x0a /* Public */,
-      13,    1,   78,    2, 0x0a /* Public */,
-      14,    0,   81,    2, 0x08 /* Private */,
+       1,    0,   59,    2, 0x0a /* Public */,
+       3,    0,   60,    2, 0x0a /* Public */,
+       4,    0,   61,    2, 0x0a /* Public */,
+       5,    0,   62,    2, 0x0a /* Public */,
+       6,    1,   63,    2, 0x0a /* Public */,
+       8,    0,   66,    2, 0x0a /* Public */,
+       9,    1,   67,    2, 0x0a /* Public */,
+      11,    1,   70,    2, 0x0a /* Public */,
+      13,    1,   73,    2, 0x0a /* Public */,
 
  // slots: parameters
     QMetaType::Void,
@@ -91,7 +88,6 @@ static const uint qt_meta_data_MainDialog[] = {
     QMetaType::Void, QMetaType::Int,   10,
     QMetaType::Void, QMetaType::QString,   12,
     QMetaType::Void, QMetaType::QString,   12,
-    QMetaType::Void,
 
        0        // eod
 };
@@ -141,13 +137,13 @@ int MainDialog::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 10)
+        if (_id < 9)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 10;
+        _id -= 9;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 10)
+        if (_id < 9)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 10;
+        _id -= 9;
     }
     return _id;
 }
